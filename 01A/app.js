9482e235a50e1bd4c725de2073c319c133ca3396
jay gleijsteen <36629@ma-web.nl>
2023-11-21 14:53:51 +0100

    kippen2

--- 01A/app.js
+++ 01A/app.js
@@ -5,6 +5,22 @@ class App
     {
         console.log("Hello world!")
         //code gaat hier
+       let appNaam = "luigi"
+       let versienummer = "0,328"
+       let versiedatum  = "oktober 5"
+       let autheur = "J.K rowling"
+       let copyright = "snollebolekkes"
+       let distributeur = "spongebob"
+       let darkmode = "true"
+        
+       console.log(appNaam)
+       console.log(versienummer)
+       console.log(versiedatum)
+       console.log(autheur)
+       console.log(copyright)
+       console.log(distributeur)
+       console.log(darkmode)
+
     }
 }
 
