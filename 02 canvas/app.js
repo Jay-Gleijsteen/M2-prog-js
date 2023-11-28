df4ac9083dd15a71dc1c20d27dddc11414716f6c
jay gleijsteen <36629@ma-web.nl>
2023-11-28 14:20:20 +0100

    kaas

---
+++ 02 canvas/app.js
@@ -0,0 +1,17 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!")
+        let canvas = document.getElementById("canvasId")
+        let g = canvas.getContext("2d");
+        g.fillStyle = "yellow";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "blue";
+        g.fillRect(0,0,10,10);
+        console.log(canvas)
+    }
+}
+
+let app = new App();
+app.runApplication();
