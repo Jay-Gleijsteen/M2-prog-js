1a43f73037d9c7b2b4cae7624cf234d26a211fcc
jay gleijsteen <36629@ma-web.nl>
2023-12-19 15:20:27 +0100

    commit cool

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,42 +1,43 @@
-function superMooieGlobalFunction()
+function heeftEenResultaat()
 {
-    console.log("ik ben global")
-    console.log("dus je mag mij overal aanroepen")
+    let resultaat =1;
+    return resultaat
 
 }
-superMooieGlobalFunction()
 
+let hetResultaat = heeftEenResultaat();
+console.log(hetResultaat);
+console.log(heeftEenResultaat());
 
+let x = 9;
+let a =3;
+let b =4;
+let c =89;
+    
+let y = (a*(x*x) )+ (b*x) +c;
+console.log(y);
 
-
-
-
-class App
+function ax2bcWiskunde(x,a,b,c,)
 {
-    runApplication()
-    {
-        console.log("helloworld");
-        superMooieGlobalFunction()
-    }
+    let y = (a*(x*x) )+ (b*x) +c;
+    return y
+}
+let y1 = ax2bcWiskunde(9,3,4,89)
+console.log(y1);
+
+let y2 = ax2bcWiskunde(3,6,5,45)
+console.log(y2);
+
+let y3 = ax2bcWiskunde(7,1,2,13)
+console.log(y3);
+
+function kippenpoot3(u,h,l)
+{
+
+let logY = Math.log(l) + h + Math.pow(u,2);
+console.log(logY);
 }
 
-let app = new App();
-app.runApplication();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function aanroepVoorbeeld(){
-
-}
+kippenpoot3(9,8,15)
+kippenpoot3(5,6,7)
+kippenpoot3(8,9,20)
